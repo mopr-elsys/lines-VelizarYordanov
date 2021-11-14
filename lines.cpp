e1f941cbfaa9decc9a6395af8e55ef441ae2ddc6
VelizarYordanov <77102575+VelizarYordanov@users.noreply.github.com>
2021-11-14 18:04:46 +0200

    Update lines.cpp

--- lines.cpp
+++ lines.cpp
@@ -1,48 +1,71 @@
 #include <iostream>
-#include "geom.hpp"
+#include <cmath>
 using namespace std;
 
-int main () {
-    char input;
-    Point p1,p2;
-    Line l1,l2;
-    cin>> input;
-    switch(input) {
-    case 'c': // construct
-        cin >> input;
-        switch(input) {
-        case 'l': // line from two points
-            cin >> p1 >> p2;
-            cout << Line(p1, p2) << endl;
-            break;
-        case 'c': // collinear line from line and a point
-            cin >> l1;
-            cin >> p1;
-            cout << l1.parallel(p1) << endl;
-            break;
-        case 'o': // orthogonal/perpendicular line from a line and a point
-            cin >> l1;
-            cin >> p1;
-            cout << l1.perpendicular(p1) << endl;
-            break;
-        }
-        break;
-    case 'v': // verify
-        cin >> input;
-        switch(input) {
-        case 'c': // if two lines are parallel
-            cin >> l1;
-            cin >> l2;
-            cout << boolalpha << l1.parallel(l2) << endl;
-            break;
-        case 'p': // if two lines are perpendicular
-            cin >> l1;
-            cin >> l2;
-            cout << boolalpha << l1.perpendicular(l2) << endl;
-            break;
-        }
-        break;
+bool eq(double d1, double d2, double eps = 0.001) {
+    return abs(d2 - d1) < eps;
+}
+
+struct Point {
+    double x, y;
+
+    Point(double x = 0, double y = 0) : x(x), y(y) {}
+
+    void print(ostream& out) const {
+        out<< "("<< x<< ", "<< y<< ")";
+    }
+};
+
+ostream& operator<<(ostream& out, const Point& p) {
+    p.print(out);
+    return out;
+}
+
+istream& operator>>(istream& in, Point& p) {
+    in >> p.x;
+    in >> p.y;
+    
+    return in;
+}
+
+struct Line {
+    double A, B, C;
+    Point p;
+
+    Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {
     }
 
-    return 0;
+    Line(const Point& p1, const Point& p2) {
+        A = p1.y - p2.y;
+        B = p2.x - p1.x;
+        C = -(A * p1.x + B * p1.y);
+    }
+
+    bool parallel(const Line& other) const {
+        return eq(this->A * other.B, this->B * other.A) && this->A * other.C != other.A * this->C;
+    }
+
+    Line parallel(const Point& p) {
+        return Line(this->A, this->B, -(this->A * p.x + this->B * p.y));
+    }
+
+    bool perpendicular(const Line& other) const {
+        return eq(this->A * other.A, this->B * (-(other.B)));
+    }
+
+    Line perpendicular(const Point& p) {
+        return Line(-(this->B), this->A, this->B * p.x - this->A * p.y);
+    }
+
+    void print(ostream& out) const {
+        out << A << "x + " << B << "y + " << C << " = 0";
+    }
+};
+
+istream& operator>>(istream& in, Line& l) {
+    in >> l.A;
+    in >> l.B;
+    in >> l.C;
+    
+    return in;
 }
